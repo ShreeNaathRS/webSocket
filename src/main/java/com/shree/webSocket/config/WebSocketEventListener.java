52e596894df9fc9062cef94bcb0b6560f24369ec
Shree Naath R S <shreenaath.rs@bounteous.com>
2024-09-22 17:58:35 +0530

    userName taken from header

--- src/main/java/com/shree/webSocket/config/WebSocketEventListener.java
+++ src/main/java/com/shree/webSocket/config/WebSocketEventListener.java
@@ -1,16 +1,25 @@
 package com.shree.webSocket.config;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import com.shree.webSocket.dto.ChatMessage;
 import com.shree.webSocket.enums.MessageType;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
@@ -18,21 +27,55 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class WebSocketEventListener {
 	
+	@Getter
+	@Setter
+	@AllArgsConstructor
+	class NativeHeader {
+		private String userName;
+	}
+	
 	@Autowired
 	private final SimpMessageSendingOperations messageSendingOperations;
 
 	@EventListener
 	public void webSocketDisconnectListener(SessionDisconnectEvent disconnectEvent) {
-		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(disconnectEvent.getMessage());
-		String userName = (String) headerAccessor.getSessionAttributes().get("userName");
+//		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(disconnectEvent.getMessage());
+//		String userName = getUserNameFromStompHeaderAccessor(headerAccessor);
+//		if(null != userName) {
+//			log.info("User {} disconnected!", userName);
+//			var chatMessage = ChatMessage.builder()
+//					.type(MessageType.LEAVE)
+//					.sender(userName)
+//					.build();
+//			messageSendingOperations.convertAndSend("/topic/public", chatMessage);
+//		}
+	}
+	
+	@EventListener
+	public void webSocketDisconnectListener(SessionConnectedEvent connectedEvent) {
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(connectedEvent.getMessage());
+		String userName = getUserNameFromStompHeaderAccessor(headerAccessor);
 		if(null != userName) {
-			log.info("User {} disconnected!", userName);
+			log.info("User {} connected!", userName);
 			var chatMessage = ChatMessage.builder()
-					.type(MessageType.LEAVE)
+					.type(MessageType.JOIN)
 					.sender(userName)
+					.content(userName + " has joined the chat room")
 					.build();
 			messageSendingOperations.convertAndSend("/topic/public", chatMessage);
 		}
+	}
+	
+	private String getUserNameFromStompHeaderAccessor(StompHeaderAccessor headerAccessor) {
+		GenericMessage<?> message = (GenericMessage<?>) headerAccessor.getHeader("simpConnectMessage");
+		MessageHeaders messageHeaders = message.getHeaders();
+		if(messageHeaders.containsKey("nativeHeaders")) {
+			Map<String, ArrayList<String>> header = (Map<String, ArrayList<String>>) messageHeaders.get("nativeHeaders");
+			if(header.containsKey("userName")) {
+				return (String) header.get("userName").get(0);
+			}
+		}
+		return null;
 	}
 	
 }
